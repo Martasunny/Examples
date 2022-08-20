535d3435559707218d019307d3472cdb5fdc7fa5
Marta <[martasunnyy@gmail.com]>
2022-08-20 18:01:16 +0300

    программа приветствия

---
+++ example005_ElseIf/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "кирилл")
+{
+    Console.WriteLine("Привет, Котя");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
