d6dfaefc2a5e3d482c1dd8663408f074dbf8bcee
Marta <[martasunnyy@gmail.com]>
2022-08-27 15:16:05 +0300

    задача нахождения индекса числа

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12, 24, 37, 43, 56, 61, 74, 89, 61};
+
+int n = array.Length;
+int find = 61;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
