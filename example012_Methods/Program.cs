ed4994f5243e6b1e8df17822876049d382988518
Marta <[martasunnyy@gmail.com]>
2022-09-03 16:34:22 +0300

    дописала

--- example012_Methods/Program.cs
+++ example012_Methods/Program.cs
@@ -32,16 +32,40 @@
 //Console.WriteLine(year);
 
 //Вид 4 - что то принимают, что то возвращают
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
-    while( i < count)
+//string Method4(int count, string text)
+//{
+  //  int i = 0;
+    //string result = String.Empty;
+    //while( i < count)
+   // {
+    //    result = result + text;
+   //     i++;
+   // }
+   // return result;
+//}
+//string res = Method4(10, "a,s,d,f");
+//Console.WriteLine(res);
+
+//Цикл For
+
+//string Method4(int count, string text)
+//{
+  //  string result = String.Empty;
+    //for (int i = 0; i < count; i++)
+    //{
+      //  result = result + text;
+    //}
+    //return result;
+//}
+//string res = Method4(10, "a,s,d,f");
+//Console.WriteLine(res);
+
+//Цикл в цикле
+ for (int i = 2; i <= 10; i++)
+ {
+    for (int j = 2; j < 10; j++)
     {
-        result = result + text;
-        i++;
+        Console.WriteLine($"{i} * {j} = {i*j}");
     }
-    return result;
-}
-string res = Method4(10, "a,s,d,f");
-Console.WriteLine(res);
+    Console.WriteLine();
+ }
